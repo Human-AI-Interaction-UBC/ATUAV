458b964a097f05fdf61052bb500d3b607171d79e
oschmid <oliver.schmd@gmail.com>
2012-06-28 13:21:50 -0700

    window size as command line parameter

--- RealTimeProcessing/ATUAV_RT/Program.cs
+++ RealTimeProcessing/ATUAV_RT/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using NDesk.Options;
 using Tobii.Eyetracking.Sdk;
 using Tobii.Eyetracking.Sdk.Time;
 
@@ -12,15 +13,52 @@ namespace ATUAV_RT
     /// Requires:
     /// - Tobii SDK 3.0 RC 1 http://www.tobii.com/en/eye-tracking-research/global/landingpages/analysis-sdk-30rc/
     /// - FixDet http://www.sis.uta.fi/~csolsp/projects.php
+    /// - NDesk.Options http://www.ndesk.org/Options
     /// </summary>
     class Program
     {
         private static Clock clock;
+        private static int windowDuration = 3000; // Default is 3000 ms
+        private static bool help = false;
 
+        /// <summary>
+        /// Parses arguments and finds connected Eyetrackers.
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
         static void Main(string[] args)
         {
             Library.Init();
             clock = new Clock();
+
+            // parse arguments
+            var p = new OptionSet()
+            {
+                { "w|window=", "the {DURATION} of a window in ms (default=" + windowDuration + ")", (int v) => windowDuration = v },
+                { "h|help", "", v => help = v != null }
+            };
+
+            try
+            {
+                List<string> extra = p.Parse(args);
+                if (extra.Count > 0)
+                {
+                    Console.WriteLine("Unknown arguments.");
+                    Console.WriteLine();
+                    help = true;
+                }
+            }
+            catch (OptionException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                help = true;
+            }
+
+            if (help)
+            {
+                ShowHelp(p);
+                return;
+            }
 
             // find eyetrackers on LAN
             EyetrackerBrowser browser = new EyetrackerBrowser(EventThreadingOptions.BackgroundThread);
@@ -35,6 +73,24 @@ namespace ATUAV_RT
             }
         }
 
+        /// <summary>
+        /// Writes usage to console.
+        /// </summary>
+        /// <param name="options">Parsed command line arguments</param>
+        static void ShowHelp(OptionSet options)
+        {
+            Console.WriteLine("Usage: atuav_rt [OPTIONS]");
+            Console.WriteLine("Process eyetracking data in real time");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            options.WriteOptionDescriptions(Console.Out);
+        }
+
+        /// <summary>
+        /// Connects to found eyetrackers, synchronizes CPU and eyetracker clocks, and attaches event handlers.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">Contains found EyetrackerInfo</param>
         static void EyetrackerFound(object sender, EyetrackerInfoEventArgs e)
         {
             EyetrackerConnector connector = new EyetrackerConnector(e.EyetrackerInfo);
@@ -60,7 +116,7 @@ namespace ATUAV_RT
 
             while (true)
             {
-                Thread.Sleep(3000);
+                Thread.Sleep(windowDuration);
                 printer.RenewWindow(true);
             }
         }
