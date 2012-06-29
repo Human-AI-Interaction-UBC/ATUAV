6e023d45aab0b81cf5675e3634c3f14cb3284f56
oschmid <oliver.schmd@gmail.com>
2012-06-29 16:38:27 -0700

    validity checking in fixation detector

--- RealTimeProcessing/ATUAV_RT/GazeDataHandlers/FixationDetector.cs
+++ RealTimeProcessing/ATUAV_RT/GazeDataHandlers/FixationDetector.cs
@@ -26,9 +26,7 @@ namespace ATUAV_RT
         private readonly FixDetector fixationDetector;
 
         /// <summary>
-        /// Initializes default fixation detector to Tobii Studio default **TODO**
-        /// Analyzer = EFDAnalyzer.fdaFixationSize
-        /// Filter = EFDFilter.fdfAveraging
+        /// Initializes fixation detector to Tobii Studio default
         /// </summary>
         /// <param name="syncManager"></param>
         public FixationDetector(SyncManager syncManager) : base(syncManager)
@@ -37,21 +35,15 @@ namespace ATUAV_RT
             fixationDetector.init();
 
             // TODO determine settings of Tobii Fixation Filter
+            // Detailed information available in Tobii Studio User Manual 1.X
+            // http://www.tobii.com/Global/Analysis/Downloads/User_Manuals_and_Guides/Tobii_Studio1.X_UserManual.pdf
+            // as well as the appendix of Olsson, P. 2007. Real-time and offline filters for eye tracking. Msc thesis, KTH Royal Institue of Technology, April 2007.
             fixationDetector.Analyzer = EFDAnalyzer.fdaFixationSize;
-            fixationDetector.Filter = EFDFilter.fdfNone;
+            fixationDetector.Filter = EFDFilter.fdfAveraging;
             fixationDetector.setAnalyzerProperty("fixation_radius", 35);
             fixationDetector.setAnalyzerProperty("noise_filter", 1);
-            //fixationDetector.MinFixDuration = 17;
-
-            // alt: Raw Data Filter
-
-            // alt: ClearView Fixation Filter
-            /*
-            fixationDetector.Analyzer = EFDAnalyzer.fdaFixationSize;
-            fixationDetector.Filter = EFDFilter.fdfNone;
-            fixationDetector.setAnalyzerProperty("fixation_radius", 50);
-            fixationDetector.MinFixDuration = 100;
-            */
+            fixationDetector.FilterBufferSize = 5;
+            fixationDetector.MinFixDuration = 0;
         }
 
         ~FixationDetector()
@@ -79,19 +71,35 @@ namespace ATUAV_RT
         /// <param name="e"></param>
         protected override void GazeDataReceivedSynchronized(object sender, GazeDataEventArgs e)
         {
-            int time = (int)syncManager.RemoteToLocal(e.GazeDataItem.TimeStamp);
+            // ignore gaze data with low validity
+            if (e.GazeDataItem.LeftValidity < 2 || e.GazeDataItem.RightValidity < 2)
+            {
+                int time = (int)syncManager.RemoteToLocal(e.GazeDataItem.TimeStamp);
 
-            // calculate coordinates in pixels (0, 0 is top left corner
-            double leftX = e.GazeDataItem.LeftGazePoint2D.X * SCREEN_WIDTH;
-            double leftY = e.GazeDataItem.LeftGazePoint2D.Y * SCREEN_HEIGHT;
-            double rightX = e.GazeDataItem.RightGazePoint2D.X * SCREEN_WIDTH;
-            double rightY = e.GazeDataItem.RightGazePoint2D.Y * SCREEN_HEIGHT;
+                // convert normalized screen coordinates (float between [0 - 1]) to pixel coordinates
+                // coordinates (0, 0) designate the top left corner
+                double leftX = e.GazeDataItem.LeftGazePoint2D.X * SCREEN_WIDTH;
+                double leftY = e.GazeDataItem.LeftGazePoint2D.Y * SCREEN_HEIGHT;
+                double rightX = e.GazeDataItem.RightGazePoint2D.X * SCREEN_WIDTH;
+                double rightY = e.GazeDataItem.RightGazePoint2D.Y * SCREEN_HEIGHT;
 
-            // average left and right eyes
-            int x = (int) ((leftX + rightX) / 2);
-            int y = (int) ((leftY + rightY) / 2);
+                // average left and right eyes
+                if (e.GazeDataItem.LeftValidity < 2 && e.GazeDataItem.RightValidity < 2)
+                {
+                    int x = (int)((leftX + rightX) / 2);
+                    int y = (int)((leftY + rightY) / 2);
 
-            fixationDetector.addPoint(time, x, y);
+                    fixationDetector.addPoint(time, x, y);
+                }
+                else if (e.GazeDataItem.LeftValidity < 2)
+                {
+                    fixationDetector.addPoint(time, (int)leftX, (int)leftY);
+                }
+                else if (e.GazeDataItem.RightValidity < 2)
+                {
+                    fixationDetector.addPoint(time, (int)rightX, (int)rightY);
+                }
+            }
         }
     }
 }
