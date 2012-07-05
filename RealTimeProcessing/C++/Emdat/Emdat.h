90a43c45274ecc23800dc78595603a2e3095e43a
oschmid <oliver.schmd@gmail.com>
2012-07-05 13:56:28 -0700

    linked C++ connector to python26.lib

--- RealTimeProcessing/C++/Emdat/Emdat.h
+++ RealTimeProcessing/C++/Emdat/Emdat.h
@@ -6,6 +6,15 @@
 using namespace std;
 using namespace System;
 
+// .../Python26/libs/python26_d.lib (DEBUG version of python)
+// the proper solution is probably to install debug python from source
+// but the quick solution (which I used) is to disable python debugging
+// and use .../Python26/libs/python26.lib
+// To do this .../Python26/include/pyconfig.h must be changed
+// search for _DEBUG, there should be 2 instances, for these 2 instances
+// make sure: 
+// 1) python26.lib is used in place of python26_d.lib
+// 2) Py_DEBUG is not defined
 namespace Emdat {
 	// calls EMDAT python library
 	class UnmanagedEmdatModule {
