4870b808bda11f4cf532662fa82d47fd986c58b2
oschmid <oliver.schmd@gmail.com>
2012-10-31 14:24:04 -0700

    track and sum numfixation features

--- ATUAV_Experiment/ATUAV_Experiment/Infovis/observer.js
+++ ATUAV_Experiment/ATUAV_Experiment/Infovis/observer.js
@@ -1,16 +1,37 @@
-﻿var address = "http://localhost:8080/atuav"
-var id = "observer"
+﻿var address = "http://localhost:8080/atuav";
+var id = "observer";
+var trackedSumFeatures = []
 
 function pollFeatures() {
-    $.getJSON(address + "/features?id=" + id + "&callback=?", null, function(data) {
+    $.getJSON(address + "/features?id=" + id + "&callback=?", null, function (data) {
         var features = "";
         for (var key in data) {
             if (data.hasOwnProperty(key)) {
                 features += (key + ": " + data[key] + "<br/>");
+                if ($.inArray(key, trackedSumFeatures) >= 0) {
+                    updateTrackedSumFeature(key, data[key]);
+                }
             }
         }
         $("#features").html(features);
     });
 }
 
+function addTrackedSumFeatures() {
+    $(".sum").each(function (index, element) {
+        trackedSumFeatures[index] = element.id;
+    })
+}
+
+function updateTrackedSumFeature(feature, value) {
+    $feature = $("#" + feature);
+    if ($feature.text() != "") {
+        value = (parseInt($feature.text().match(/\d+/)) + parseInt(value));
+    }
+    $feature.text("sum " + feature + ": " + value);
+}
+
 setInterval(pollFeatures, 5000);
+$(document).ready(function () {
+    addTrackedSumFeatures();
+});
