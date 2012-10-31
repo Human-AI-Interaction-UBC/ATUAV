4d2129074043044cd74f0f33d3dfa1b0e77deae8
oschmid <oliver.schmd@gmail.com>
2012-10-31 10:35:03 -0700

    separated observer html, js and css

---
+++ ATUAV_Experiment/ATUAV_Experiment/Infovis/observer.js
@@ -0,0 +1,15 @@
+﻿var address = "http://localhost:8080/atuav"
+
+function pollFeatures() {
+    $.getJSON(address + "/features?id=observer&callback=?", null, function (data) {
+        var value_string = "";
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                value_string += (key + ":" + data[key] + "<br/>");
+            }
+        }
+        $("#features").html(value_string);
+    });
+}
+
+setInterval(pollFeatures, 5000);
