ae99ca60f2d2b9f292d4399c289f13dec2ba0693
oschmid <oliver.schmd@gmail.com>
2013-04-12 12:54:03 -0700

    Functional real-time eye tracker visualization.

--- ATUAV_Demo/ATUAV_Demo/Demo/observer.js
+++ ATUAV_Demo/ATUAV_Demo/Demo/observer.js
@@ -2,15 +2,25 @@
 var processorId = "experiment";
 var trackedSumFeatures = []
 
+var sparkline = [];
+
 function startTask(runId, aois) {
-    $.get(address + "/start?runId=" + runId + "&aois=" + aois)
+    $.get(address + "/start?runId=" + runId + "&aois=" + aois);
+    var maxLength = 12; // Number of samples to show in the sparklines
+    sparkline["fixationrate"] = new Sparkline("fixationrate", [], 0, 0.02, maxLength);
+    sparkline["meanfixationduration"] = new Sparkline("meanfixationduration", [], 0, 1000, maxLength);
+    sparkline["meanrelpathangles"] = new Sparkline("meanrelpathangles", [], 0, 3, maxLength);
+    sparkline["stddevpathdistance"] = new Sparkline("stddevpathdistance", [], 0, 500, maxLength);
 }
 
 function pollFeatures() {
-    $.getJSON(address + "/features?processorId=" + processorId + "&callback=?", null, function(data) {
+    $.getJSON(address + "/features?processorId=" + processorId + "&callback=?", null, function (data) {
         var features = "";
         for (var key in data) {
             if (data.hasOwnProperty(key)) {
+                if (key in sparkline && key != "length") {
+                    sparkline[key].addData([Number(data[key])]);
+                }
                 features += (key + ": " + data[key] + "<br/>");
                 if ($.inArray(key, trackedSumFeatures) >= 0) {
                     updateTrackedSumFeature(key, data[key]);
@@ -44,6 +54,6 @@ function updateTrackedSumFeature(feature, value) {
 $(document).ready(function () {
     addTrackedSumFeatures();
     startTask(0, "left\\t0,0\\t640,0\\t640,1024\\t0,1024\\r\\nright\\t641,0\\t1280,0\\t1280,1024\\t641,1024")
-    setInterval(pollFeatures, 5000);
+    setInterval(pollFeatures, 1000);
     //setInterval(pollShowText, 1000);
 });
